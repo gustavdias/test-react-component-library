00b16608e77b3d53cde494b234340e7309882beb
gustavodias <gustavodias@itera.com.br>
2021-03-22 12:42:16 +0300

    first commit

---
+++ storybook/src/components/Requirements/index.js
@@ -0,0 +1,2 @@
+export * from './Requirements';
+//The requirement component is the component that I allow people to use when they install this lib 
---
+++ storybook/src/index.js
@@ -0,0 +1,2 @@
+export * from './components/Requirements';
+//job of index is to export all components for any app to use the components
